45c40ab95816bf2e33522d3cb4d19903b08657a9
Cochard <zarmion@gmail.com>
2017-12-28 22:16:57 +0100

    et voici le pain !!!

--- src/items/consommables/Pain.java
+++ src/items/consommables/Pain.java
@@ -1,5 +1,47 @@
 package items.consommables;
 
-public class Pain {
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+import character.Entitee;
+import effects.Effect;
+import effects.RestoreHeal;
+import effects.RestoreMana;
+
+public class Pain extends Consommable{
+	
+	
+	public Pain()
+	{
+		super();
+		this.fight = false;
+		this.effects.add(new RestoreHeal(15, null));
+		this.effects.add(new RestoreMana(15, null));
+		this.name="Pain";
+		try {
+			this.icone = new Image("resources/Icones/Bread.png");
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+		
+		this.stacks = 3;
+	}
+	
+	
+	@Override
+	public void utiliser(String log, Entitee cible)
+	{
+		for(Effect e : this.effects)
+		{
+			e.changeTarget(cible);
+			e.activer(log);
+		}
+		
+		if(this.stacks>0)
+			this.stacks --;
+		
+		
+		System.out.println(log);
+	}
 
 }
